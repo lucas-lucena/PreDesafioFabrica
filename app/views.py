779c23bdb2225f63d3b26e85aab07c4c34070ddd
Shinobu Oshino <lucas.lucena@ccc.ufcg.edu.br>
2025-02-28 09:31:31 -0300

    :sparkles: feat: Alterando view em views.py para renderizar home.html

--- app/views.py
+++ app/views.py
@@ -1,6 +1,12 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello World")
+    return render(request, 'app/home.html')
+
+
+### Codigo criado apenas para exibir mensagem inicial em no navegador
+# from django.http import HttpResponse
+#
+# def home(request):
+#     return HttpResponse("Hello World")
